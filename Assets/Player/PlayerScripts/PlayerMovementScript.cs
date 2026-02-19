b76332d14f7688a4b7525604d9b1dc32a6ce9684
Infiltrate101 <anton.nijkamp@gmail.com>
2026-02-19 11:44:44 -0500

    Added Sprinting and Proper Camera
    Game now allows sprinting while holding shift, and the camera now moves with the mouse for better movement. Added some platforms in the sample area to test.

--- Assets/Player/PlayerScripts/PlayerMovementScript.cs
+++ Assets/Player/PlayerScripts/PlayerMovementScript.cs
@@ -6,33 +6,63 @@ public class PlayerMovementScript : MonoBehaviour
 {
     public Rigidbody rb;
     public float moveSpeed;
+    public float sprintSpeed;
     public float rotateSpeed;
     public float jumpHeight;
     public InputActionReference move;
     public InputActionReference jump;
+    public InputActionReference sprint;
     public Animator animator;
-    public CharacterController controller;
     
     private Vector3 _moveDirection;
     private bool _isGrounded;
     private bool _jumpPressed;
+    private bool _isSprinting;
     
     private void Update()
     {
-        _moveDirection = move.action.ReadValue<Vector3>();
+        Vector2 input = move.action.ReadValue<Vector2>();
+
+        Transform cam = Camera.main.transform;
+        
+        Vector3 cameraForward = cam.forward;
+        cameraForward.y = 0f;
+        cameraForward.Normalize();
+
+        Vector3 cameraRight = cam.right;
+        cameraRight.y = 0f;
+        cameraRight.Normalize();
+        
+        _moveDirection = cameraForward * input.y + cameraRight * input.x;
+
+        if (_moveDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(_moveDirection, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                targetRotation,
+                rotateSpeed * Time.deltaTime
+            );
+        }
 
         if (jump.action.WasPressedThisFrame() && _isGrounded)
             _jumpPressed = true;
+        
+        _isSprinting = sprint.action.IsPressed() && _isGrounded;
+        animator.SetBool("isSprinting", _isSprinting);
     }
 
     private void FixedUpdate()
     {
+        float speed = _isSprinting ? sprintSpeed : moveSpeed;
+        
         rb.linearVelocity = new Vector3(
-            _moveDirection.x * moveSpeed,
+            _moveDirection.x * speed,
             rb.linearVelocity.y,
-            _moveDirection.z * moveSpeed
+            _moveDirection.z * speed
         );
-
+        
         if (_moveDirection != Vector3.zero)
         {
             animator.SetBool("isWalking", true);
@@ -51,9 +81,13 @@ public class PlayerMovementScript : MonoBehaviour
             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
             _isGrounded = false;
             _jumpPressed = false;
+            animator.SetBool("jumpRequested", true);
         }
 
-        //if (rb.linearVelocity.y < 0);
+        if (rb.linearVelocity.y < 0)
+        {
+            animator.SetBool("jumpRequested", false);
+        }
     }
     
     void OnCollisionEnter(Collision collision)
@@ -71,6 +105,7 @@ public class PlayerMovementScript : MonoBehaviour
         {
             _isGrounded = false;
             animator.SetBool("isGrounded", false);
+            animator.SetBool("isWalking", false);
         }
     }
 }
