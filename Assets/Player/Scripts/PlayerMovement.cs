ba457b67477004166dafe70029997d891a8c7f9c
Infiltrate101 <anton.nijkamp@gmail.com>
2026-02-26 18:10:49 -0500

    Added Slope Movement

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -23,11 +23,13 @@ namespace Player.Scripts
         public float jumpForce;
         public float jumpCooldown;
         public float airMultiplier;
+        
         private bool _readyToJump;
 
         [Header("Crouch")] 
         public float crouchSpeed;
         public float crouchYScale;
+        
         private float _startYScale;
         
         [Header("Keybindings")]
@@ -39,7 +41,14 @@ namespace Player.Scripts
         [Header("Ground Check")] 
         public float playerHeight;
         public LayerMask whatIsGround;
+        
         private bool _isGrounded;
+        
+        [Header("Slope Handling")]
+        public float maxSlopeAngle;
+
+        private bool _exitingSlope;
+        private RaycastHit _slopeHit;
 
         public enum MovementState
         {
@@ -146,33 +155,53 @@ namespace Player.Scripts
         {
             _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
 
-            switch (_isGrounded)
+            //On slope
+            if (OnSlope() && !_exitingSlope)
             {
-                //On ground
-                case true:
-                    _rb.AddForce(_moveDirection.normalized * (_speed * 10f), ForceMode.Force);
-                    break;
-                //In air
-                case false:
-                    _rb.AddForce(_moveDirection.normalized * (_speed * 10f * airMultiplier), ForceMode.Force);
-                    break;
+                _rb.AddForce(GetSlopeMoveDirection() * (_speed * 20f), ForceMode.Force);
+                
+                if(_rb.linearVelocity.y > 0)
+                    _rb.AddForce(Vector3.down * 80f, ForceMode.Force);
             }
+            else if (_isGrounded)
+            {
+                _rb.AddForce(_moveDirection.normalized * (_speed * 10f), ForceMode.Force);
+            }
+            else if (!_isGrounded)
+            {
+                _rb.AddForce(_moveDirection.normalized * (_speed * 10f * airMultiplier), ForceMode.Force);
+            }
+            
+            //Turn off gravity when on a slope
+            _rb.useGravity = !OnSlope();
         }
 
         private void SpeedControl()
         {
-            Vector3 flatVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
-
-            //Limit velocity
-            if (flatVelocity.magnitude > _speed)
+            //Limiting speed on slopes
+            if (OnSlope() && !_exitingSlope)
             {
-                Vector3 limitedVelocity = flatVelocity.normalized * _speed;
-                _rb.linearVelocity = new Vector3(limitedVelocity.x, _rb.linearVelocity.y, limitedVelocity.z);
+                if (_rb.linearVelocity.magnitude > _speed)
+                    _rb.linearVelocity = _rb.linearVelocity.normalized * _speed;
+            }
+            //Limit speed on ground or air
+            else
+            {
+                Vector3 flatVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
+
+                //Limit velocity
+                if (flatVelocity.magnitude > _speed)
+                {
+                    Vector3 limitedVelocity = flatVelocity.normalized * _speed;
+                    _rb.linearVelocity = new Vector3(limitedVelocity.x, _rb.linearVelocity.y, limitedVelocity.z);
+                }
             }
         }
 
         private void Jump()
         {
+            _exitingSlope = true;
+            
             //Reset y velocity
             _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
             _rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
@@ -181,7 +210,22 @@ namespace Player.Scripts
         private void ResetJump()
         {
             _readyToJump = true;
+            _exitingSlope = false;
         }
 
+        private bool OnSlope()
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, playerHeight * 0.5f + 0.3f))
+            {
+                float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
+                return angle < maxSlopeAngle && angle != 0;
+            }
+            return false;
+        }
+
+        private Vector3 GetSlopeMoveDirection()
+        {
+            return Vector3.ProjectOnPlane(_moveDirection, _slopeHit.normal).normalized;
+        }
     }
 }
